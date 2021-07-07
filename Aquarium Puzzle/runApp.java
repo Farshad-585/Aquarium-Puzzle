104b8f60a8cb1791ba6e858eb3b8eaab2b1a95b6
Farshad <61343458+Farshad-585@users.noreply.github.com>
2021-07-07 10:12:09 +0800

    Update runApp.java

--- Aquarium Puzzle/runApp.java
+++ Aquarium Puzzle/runApp.java
@@ -1,3 +1,9 @@
+/**
+ * Simple utility to run the puzzle.
+ * @author Farshad Ghanbari
+ * @version 2021
+ */
+
 public class runApp {
   public static void main(String[] args) {
     AquariumViewer puzzle = new AquariumViewer("Examples/a6_5.txt");
